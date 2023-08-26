69c0f5c8c37c3679b8a44cffbd238aa4575889f2
Tobias Schulz <tobias.schulz@linux.com>
2023-08-27 01:38:02 +0200

    Update crossover_reset_bottles.sh

--- crossover_reset_bottles.sh
+++ crossover_reset_bottles.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
+test -f /opt/homebrew/bin/pidof || brew install pidof
+
 PWD=/Applications/CrossOver.app/Contents/MacOS
 
 PROC_NAME=CrossOver
 
-# get all pids of CrossOver 
+# get all pids of CrossOver
 pids=(`pgrep "$PROC_NAME"`, `pidof "$PROC_NAME"`, `ps -Ac | grep -m1 '"$PROC_NAME"\$' | awk '{print \$1}'`)
 pids=`echo $pids|tr ',' ' '`
 
@@ -18,5 +20,5 @@ TIME=`date -v -1H '+%b %d, %Y, %H:%M:%S %p'`
 # modify time in order to reset trial
 plutil -replace FirstRunDate -string "$TIME" ~/Library/Preferences/com.codeweavers.CrossOver.plist
 
-# reset all bottles 
+# reset all bottles
 for file in ~/Library/Application\ Support/CrossOver/Bottles/*/.{eval,update-timestamp}; do rm -rf "$file";done
